d647cf293771ee645d6cd92f124a27dee4caae88
Jesse Moore <jmoore@affirma.com>
2022-07-06 14:41:27 -0500

    Initial Commit

---
+++ Shared/Lesson4App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson4App.swift
+//  Shared
+//
+//  Created by Affirma Interns on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Lesson4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
