3d8e97a616e386e7cd79b8e3adc6923b246c20cc
Rachel Skwerer <rskwerer@affirma.com>
2022-07-06 15:45:22 -0500

    content cards created

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ZStack {
+                Image("toronto").cornerRadius(12.0)
+                
+                VStack {
+                    Text("CN Tower")
+                    Text("Toronto")
+                        .font(.caption)
+                        
+                }.padding().background(Color.black).foregroundColor(Color.white).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/).cornerRadius(/*@START_MENU_TOKEN@*/12.0/*@END_MENU_TOKEN@*/)
+            }
+            
+            Spacer()
+            ZStack {
+                Image("london").cornerRadius(12.0)
+                
+                VStack {
+                    Text("Big Ben")
+                    Text("London")
+                        .font(.caption)
+                        
+                }.padding().background(Color.black).foregroundColor(Color.white).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/).cornerRadius(/*@START_MENU_TOKEN@*/12.0/*@END_MENU_TOKEN@*/)
+            }
+            
+        }
+        
+        
+
+      
     }
 }
 
